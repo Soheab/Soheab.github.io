406235609a01a92e0a975a72b5f4b5cbb5dd7a6b
Soheab <soheabdebeste@hotmail.com>
2019-06-11 19:05:16 +0200

    updated to source and some changes to 2.0 commands.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,5 @@
 window.addEventListener('scroll', function() {
+  // Hide arrow button when scrolling down the page
   var scrollPosition = window.scrollY;
   var logoContainer = document.getElementsByClassName('arrow')[0];
   if (scrollPosition >= 100) logoContainer.classList.add('arrow--scrolled');
