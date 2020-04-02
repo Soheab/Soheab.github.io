c384dbdb3bb086da48fcff86b5b422175b5b5135
Soheab <soheabdebeste@hotmail.com>
2020-04-02 22:58:41 +0200

    looks very nice yes

--- README.md
+++ README.md
@@ -1,7 +1,10 @@
 # alexflipnote.dev
 
-- Site: https://soheab.github.io/sparklybot/
-- Usage: Website for Sparklybot!
+- Site: https://alexflipnote.dev
+- Usage: Information about myself
 - Framework:
   - [ModestaCSS](https://github.com/AlexFlipnote/ModestaCSS)
   - [smoothScroll](https://github.com/alicelieutier/smoothScroll)
+
+## Open-source
+Just download and do whatever you want with it, I don't really mind, but please give credits, thanks ❤
--- assets/js/main.js
+++ assets/js/main.js
@@ -5,3 +5,31 @@ window.addEventListener('scroll', function() {
   if (scrollPosition >= 100) logoContainer.classList.add('arrow--scrolled');
   else logoContainer.classList.remove('arrow--scrolled');
 });
+
+function getParameterByName(name, url) {
+  if (!url) url = window.location.href;
+  name = name.replace(/[\[\]]/g, '\\$&');
+  var regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
+  results = regex.exec(url);
+  if (!results) return null;
+  if (!results[2]) return '';
+  return decodeURIComponent(results[2].replace(/\+/g, ' '));
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+  // Load the time since I coded
+  var years_since = new Date("2015-08-01").getTime();
+  var today = new Date().getTime();
+
+  var ms = today - years_since;
+  var m = Math.floor(ms / 60000);
+  var h = Math.floor(m / 60);
+  var d = Math.floor(h / 24);
+  var y = Math.floor(d / 365);
+
+  document.getElementById("years-of-coding").innerText = y + " years";
+
+  if (getParameterByName('old')) {
+    document.querySelector('head').innerHTML += '<link href="assets/css/classic.css" type="text/css" rel="stylesheet">';
+  }
+});
