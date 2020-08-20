a3c9151a09ef2478f07a4e6ffc35025f299a499d
Soheab <soheabdebeste@hotmail.com>
2020-08-20 18:39:57 +0200

    big cleanup and command updates.

--- assets/js/main.js
+++ assets/js/main.js
@@ -27,3 +27,16 @@ document.addEventListener('DOMContentLoaded', function () {
 
     document.getElementById("years-of-coding").innerText = y + " years"
 })
+
+
+window.addEventListener('scroll', function () {
+    var scrollPosition = window.scrollY;
+    var logoContainer = document.getElementsByClassName('arrow')[0];
+
+    scrollPosition = window.scrollY;
+    if (scrollPosition >= 100) logoContainer.classList.add('arrow--scrolled');
+    else logoContainer.classList.remove('arrow--scrolled');
+});
+
+
+
